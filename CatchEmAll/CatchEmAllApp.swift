b5302d24fb1937fa9f769336752eee8c99305216
Matthew Peterson <matthew251125@icloud.com>
2025-11-30 11:26:27 -0600

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Matthew Peterson on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
